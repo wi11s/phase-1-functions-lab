920a33b0f5632a782c634b35a3afb3122857e4ec
William <wms274@nyu.edu>
2022-08-30 11:53:00 -0400

    commit tree 24f0a0860dd011b099b9f16d9339e921ac015ac4

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(startBlock) {
+    return (Math.abs(startBlock-42));
+}
+
+function distanceFromHqInFeet(startBlock) {
+    return distanceFromHqInBlocks(startBlock)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+   return Math.abs((start-destination)*264)
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination)<=400) {
+        return 0
+    }
+    if (distanceTravelledInFeet(start, destination)<2000) {
+        return (distanceTravelledInFeet(start, destination)-400)*.02;
+    }
+    if (distanceTravelledInFeet(start, destination)>2000 && distanceTravelledInFeet(start, destination)<2500) {
+        return 25;
+    }
+    if (distanceTravelledInFeet(start, destination)>2500) {
+        return "cannot travel that far"
+    }
+}
